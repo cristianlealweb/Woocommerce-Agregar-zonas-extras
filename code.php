555e4ea696215897fd2904badde24bd73567c0e0
Cristian Leal <65972364+cristianlealweb@users.noreply.github.com>
2020-07-29 21:03:20 -0300

    Create code.php

---
+++ code.php
@@ -0,0 +1,25 @@
+<?php
+// Añadir codigo en function.php
+add_filter('woocommerce_states', 'add_custom_states_to_country');
+add_filter('woocommerce_countries_allowed_country_states', 'add_custom_states_to_country');
+function add_custom_states_to_country( $states ) {
+    $states['AR'] = array(
+        'ET' => __('El Talar', 'woocommerce'),', 'woocommerce'),
+        'SF' => __('San Fernando', 'woocommerce'),
+        'DT' => __('Don Torcuato', 'woocommerce'),
+        'SI' => __('San Isidro', 'woocommerce'),
+        'MZ' => __('Martínez', 'woocommerce'),
+        'BO' => __('Boulogne', 'woocommerce'),
+        'MN' => __('Munro', 'woocommerce'),
+        'VB' => __('Villa Ballester', 'woocommerce'),
+        'OM' => __('Ombú', 'woocommerce'),
+        'SM' => __('San Martín', 'woocommerce'),
+        'CH' => __('Churruca', 'woocommerce'),
+        'EP' => __('El Palomar', 'woocommerce'),
+        'CA' => __('Caseros', 'woocommerce'),
+        'SL' => __('Santos Lugares', 'woocommerce'),
+    );
+    return $states;
+}
+
+?>
